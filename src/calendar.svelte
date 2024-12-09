43c6b01e2b372ef86b767f3e66065d7a4eb5882f
meteohr <felix_o@hotmail.de>
2024-12-09 21:43:04 +0100

    initial commit :^)

---
+++ src/calendar.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+	const isLeapYear = (year: number) => {
+		if (year % 4 !== 0) return false;
+		if (year % 100 !== 0) return true;
+		return year % 400 === 0;
+	};
+	const months = [
+		{ name: "January", days: 31, start: 3 },
+		{ name: "February", days: isLeapYear(2024) ? 29 : 28, start: 6 },
+		{ name: "March", days: 31, start: 6 },
+		{ name: "April", days: 30, start: 2 },
+		{ name: "May", days: 31, start: 4 },
+		{ name: "June", days: 30, start: 0 },
+		{ name: "July", days: 31, start: 2 },
+		{ name: "August", days: 31, start: 5 },
+		{ name: "September", days: 30, start: 1 },
+		{ name: "October", days: 31, start: 3 },
+		{ name: "November", days: 30, start: 6 },
+		{ name: "December", days: 31, start: 1 }
+	];
+	const getDay = (month: string, day: number): string | undefined => {
+		const date = new Date(`${month} ${day}, 2024 12:00:00`);
+		const dayNumber = date.getDay();
+		if (isNaN(dayNumber)) return;
+		const dayMap = {
+			0: "So",
+			1: "Mo",
+			2: "Di",
+			3: "Mi",
+			4: "Do",
+			5: "Fr",
+			6: "Sa"
+		};
+		return dayMap[dayNumber as keyof typeof dayMap];
+	};
+</script>
+
+<h1>2024</h1>
+<div class="cal">
+	{#each months as month}
+		<div class="cal-month">
+			<h2>{month.name}</h2>
+			<div class="cal-days">
+				{#each Array.from({length: month.days}, (_, i) => i + 1) as day}
+					<div class="toggle-wrapper">
+						<input class="toggle" type="checkbox" id="{month.name}-{day}-toggle">
+						<label for="{month.name}-{day}-toggle" class="toggle-label">{getDay(month.name, day)}</label>
+					</div>
+				{/each}
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.cal {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+	}
+	.cal-month {
+		border: 1px solid black;
+		margin: 10px;
+		padding: 10px;
+	}
+	.cal-days {
+		display: grid;
+		grid-template-columns: repeat(7, 1fr);
+	}
+	.toggle {
+			appearance: none;
+			width: 35px;
+			height: 30px;
+			background-color: darkgrey;
+	}
+	.toggle:checked {
+			background-color: lightgreen;
+	}
+	.toggle-label {
+			position: absolute;
+			left: 8px;
+			top: 3px;
+	}
+	.toggle-wrapper {
+			position: relative;
+	}
+</style>
---
+++ src/vacation-planner.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Calendar from "./calendar.svelte";
+</script>
+
+<style>
+	.config {
+			padding: 20px;
+	}
+</style>
+
+<div class="config">
+	<h1>Urlaubs Planer</h1>
+	Jahr:
+	<select name="year" id="year">
+		<option value="2024">2024</option>
+		<option value="2025">2025</option>
+	</select>
+	Urlaubstage: <input type="number" id="vacation-days" name="vacation-days" defaultValue="30" min="0" max="100" />
+	Schulferien:
+	<select name="state" id="state">
+		<option value="bavaria">Bayern</option>
+		<option value="rlp">Rheinland-Pfalz</option>
+		<option value="berlin">Berlin</option>
+	</select>
+	<Calendar />
+</div>
