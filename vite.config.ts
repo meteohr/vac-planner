aa16ff776fef1987244c75a614f90f6ea56141bd
meteohr <felix_o@hotmail.de>
2025-05-22 23:11:28 +0200

    Trying to fix mime type issue so loading js and css works in github pages #2

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  // https://stackoverflow.com/a/76702556
+  base: '/vac-planner/',
 })
